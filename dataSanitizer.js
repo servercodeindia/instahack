d705c3d0fc96285d44630a33f14ef66a212bb9bb
servercodeindia <servercodeindia@gmail.com>
2026-08-15 19:52:56 +0530

    Update dataSanitizer.js

--- dataSanitizer.js
+++ dataSanitizer.js
@@ -1,22 +1,35 @@
-module.exports.defaultOptions(){
-return{
-sensitiveKeywords:["password","api_key"],
-replacementChar:"*"
-}
+module.exports = function (options = {}) {
+  const settings = {
+    sensitiveKeywords: ["password", "api_key"],
+    replacementChar: "*",
+    ...options
+  };
+
+  return {
+    defaultOptions() {
+      return { ...settings };
+    },
+
+    async sanitizeData(data) {
+      if (typeof data !== "string") {
+        throw new TypeError("Input must be a string");
+      }
+
+      let result = data;
+
+      for (const keyword of settings.sensitiveKeywords) {
+        const escapedKeyword = keyword.replace(
+          /[.*+?^${}()|[\]\\]/g,
+          "\\$&"
+        );
+
+        result = result.replace(
+          new RegExp(escapedKeyword, "gi"),
+          settings.replacementChar.repeat(keyword.length)
+        );
+      }
+
+      return result;
+    }
+  };
 };
-
-module.exports.sanitizeData(){
-
-try{
-
-for(const keyword of this.options.sensitiveKeywords){
-data.replace(new RegExp(keyword,'gi'),"*".repeat(keyword.length))
-}
-
-}catch(error){
-console.log(error)
-continue;
-
-}
-
-}
