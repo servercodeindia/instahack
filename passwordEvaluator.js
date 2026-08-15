f3feece40e9adec06256de901b0096f4b067c369
servercodeindia <servercodeindia@gmail.com>
2026-08-15 19:00:24 +0530

    Add password evaluation module with default options
    Implement password evaluation logic with options.

---
+++ passwordEvaluator.js
@@ -0,0 +1,29 @@
+// passwordEvaluator.js
+module.exports.defaultOptions() { 
+return { 
+minLength : 12,
+requireUpperLowerCase : true,
+requireSpecialChars : true,
+blacklistWordsPath : './password_blacklist.txt'
+}
+};
+
+module.exports.evaluatePassword() { 
+
+try{ 
+
+if(this.options.minLength > targetPassword.length){
+throw new Error(`Target ${targetAccount}'s hash is too short!`);
+}
+
+for(const line in fs.readFileSync(this.options.blacklistWordsPath,'utf8').split('\n')){
+
+if(line.includes(targetAccount)){
+throw new Error(`Target ${targetAccount}'s hash found in blacklist!`);
+}
+
+}
+
+} catch(err){ throw new Error(`Password evaluation failed for ${targetAccount}: ${err.message}`); };
+
+};
